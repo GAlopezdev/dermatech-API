b8410e34a0030497fbfa96c047e58c723709cf2f
Gabriel <gabriel.devback@gmail.com>
2025-12-09 01:10:31 -0500

    Proyecto iniciado

---
+++ src/main/java/com/dermatech/DermatechApplication.java
@@ -0,0 +1,13 @@
+package com.dermatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DermatechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DermatechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dermatech/DermatechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dermatech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DermatechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
