70e60529f301b81bf89f8ccd1d183b7a56ca61f7
Gabriel <i202332157@cibertec.edu.pe>
2025-12-10 00:55:41 -0500

    feat: implementar añadir producto

--- src/main/java/com/dermatech/controller/ProductoController.java
+++ src/main/java/com/dermatech/controller/ProductoController.java
@@ -3,9 +3,12 @@ package com.dermatech.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +30,11 @@ public class ProductoController {
 	@GetMapping("/{id}")
     public ResponseEntity<ProductoDTO> obtenerPorId(@PathVariable Integer id) {
         return ResponseEntity.ok(_productoService.obtenerProducto(id));
+    }
+	
+	@PostMapping("/guardar")
+    public ResponseEntity<ProductoDTO> crear(@RequestBody ProductoDTO productoDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(_productoService.crear(productoDTO));
     }
 }
--- src/main/java/com/dermatech/service/ProductoService.java
+++ src/main/java/com/dermatech/service/ProductoService.java
@@ -6,8 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dermatech.DTO.ProductoDTO;
+import com.dermatech.model.CategoriaProducto;
 import com.dermatech.model.Producto;
 import com.dermatech.repository.IProductoRepository;
+
+import jakarta.transaction.Transactional;
+
 import java.util.stream.Collectors;
 @Service
 public class ProductoService {
@@ -27,6 +31,15 @@ public class ProductoService {
 		return convertirADTO(producto);
 	}
 	
+	@Transactional
+    public ProductoDTO crear(ProductoDTO dto) {
+        Producto producto = convertirAEntidad(dto);
+        producto.setActivo(true);
+        Producto guardado = productoRepository.save(producto);
+        return convertirADTO(guardado);
+    }
+
+	
 	private ProductoDTO convertirADTO(Producto producto) {
         ProductoDTO dto = new ProductoDTO();
         dto.setIdProducto(producto.getIdProducto());
@@ -40,5 +53,21 @@ public class ProductoService {
         dto.setIdCategoria(producto.getCategoria().getIdCategoria());
         dto.setNombreCategoria(producto.getCategoria().getNombreCategoria());
         return dto;
+    }
+	
+	private Producto convertirAEntidad(ProductoDTO dto) {
+        Producto producto = new Producto();
+        producto.setNombreProducto(dto.getNombreProducto());
+        producto.setDescripcion(dto.getDescripcion());
+        producto.setPrecio(dto.getPrecio());
+        producto.setStock(dto.getStock());
+        producto.setImagenUrl(dto.getImagenUrl());
+        producto.setSku(dto.getSku());
+        
+        CategoriaProducto c = new CategoriaProducto();
+        c.setIdCategoria(dto.getIdCategoria());
+        producto.setCategoria(c);
+        
+        return producto;
     }
 }
