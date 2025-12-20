d30608404e4b5442416b4d9abdb5ad6d6f51e239
Gabriel <i202332157@cibertec.edu.pe>
2025-12-20 02:15:12 -0500

    feat: agregar repositorio Usuario

---
+++ src/main/java/com/dermatech/repository/IUsuarioRepository.java
@@ -0,0 +1,17 @@
+package com.dermatech.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.dermatech.model.Usuario;
+
+@Repository
+public interface IUsuarioRepository extends JpaRepository<Usuario, Integer>{
+
+	Optional<Usuario> findByEmail(String email);
+    boolean existsByEmail(String email);
+    boolean existsByDni(String dni);
+	
+}
